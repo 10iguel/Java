f8acb796bf120eb1e8ff2fcc062cac7dc44fc193
Miguel <miguel.espinoza.silloca@gmail.com>
2022-08-01 19:30:50 -0500

    Variables and If/else Statements

--- testingMore/Lab2.java
+++ testingMore/Lab2.java
@@ -8,11 +8,39 @@ public class Lab2 {
         //Declaring strings
         String name;
         String number = "15";
+        String country;
+        String age;
+        double ageInDog;
+
+        int agetoChange;
 
         //Asking
         System.out.println("Whats your name");
         name = tool.nextLine();
         System.out.printf("Hello %s! \nplease enjoy this like %s", name, number);
+
+        System.out.println("\nWhat country are you from?");
+        country = tool.nextLine();
+        System.out.printf("Nice country, I like %s\n",country);
+
+        if (country.equalsIgnoreCase("brazil")){
+            System.out.println("Personally is my favorite");
+        }else{
+            System.out.println("Not my favorite, but good");
+        }
+
+        System.out.println("What is your age?");
+        age = tool.nextLine();
+        System.out.printf("Great age, %s \n", age);
+        agetoChange  = Integer.parseInt(age);
+
+        ageInDog = agetoChange * 7;
+
+        System.out.println("You have "+ ageInDog+ " In live of the Dog");
+
+
+
+
         tool.close();
 
     }
